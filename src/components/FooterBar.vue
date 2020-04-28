f5109f255243d2866371eaa95066ac01c357746f
usamaidrsk <53287480+usamaidrsk@users.noreply.github.com>
2020-04-28 21:03:16 +0300

    navbar-footer

---
+++ src/components/FooterBar.vue
@@ -0,0 +1,34 @@
+<template>
+  <v-footer dark padless class="grey darken-4 text--darken-3" app>
+    <v-card flat tile width="100%" class="text-center">
+      <v-card-text>
+        <v-btn v-for="icon in icons" :key="icon" class="mx-4" icon>
+          <v-icon size="24px">{{ icon }}</v-icon>
+        </v-btn>
+      </v-card-text>
+
+      <v-divider></v-divider>
+
+      <v-card-text class="white--text">
+        {{ new Date().getFullYear() }} - <strong>&copy;raeder.org</strong>
+      </v-card-text>
+    </v-card>
+  </v-footer>
+</template>
+
+<script>
+export default {
+  name: "FooterBar",
+  data: function() {
+    return {
+      icons: [
+        "mdi-facebook",
+        "mdi-twitter",
+        "mdi-google-plus",
+        "mdi-linkedin",
+        "mdi-instagram"
+      ]
+    };
+  }
+};
+</script>
---
+++ src/components/NavBar.vue
@@ -0,0 +1,57 @@
+<template>
+  <div>
+    <v-navigation-drawer class="breakpoint: '' " right v-model="drawer" app>
+      <v-list>
+        <v-list-item v-for="link of links" v-bind:key="link" link>
+          <v-list-item-content>
+            <v-list-item-title> {{ link }}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+    <v-app-bar app class="grey darken-4 text--darken-3" dark>
+      <!--    <div class="" >-->
+      <v-img
+        alt="Raeder Logo"
+        class="shrink mr-2 "
+        contain
+        src="../assets/logo.png"
+        transition="scale-transition"
+        width="50"
+        id="imgstyle"
+      />
+      <!--    </div>-->
+      <v-text contain min-width="100" width="500">
+       {{name}}
+      </v-text>
+
+      <v-spacer />
+      <v-btn href="" v-for="link of links" v-bind:key="link" text>
+        {{ link }}
+      </v-btn>
+      <v-spacer />
+      <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
+    </v-app-bar>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "NavBar",
+  props: {
+    name: String
+  },
+  data: function() {
+    return {
+      links: ["home", "about", "pages", "market", "contact"],
+      drawer: false
+    };
+  }
+};
+</script>
+
+<style>
+#imgstyle {
+  border-radius: 30px;
+}
+</style>
